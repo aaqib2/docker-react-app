789bda7c78a089b46b6b1f5a0c7ac3a796ca3846
root <root@localhost.localdomain>
2018-11-24 23:12:07 -0500

    initial commit

---
+++ build/precache-manifest.7e38c1fc9769ee9ee45d4c78e479ab8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4485b91afb718058becc",
+    "url": "/static/css/main.f18a7860.chunk.css"
+  },
+  {
+    "revision": "4485b91afb718058becc",
+    "url": "/static/js/main.4485b91a.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "772a900babb8b19cba7a156457d18c44",
+    "url": "/index.html"
+  }
+];
